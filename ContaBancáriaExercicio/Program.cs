1bf91acea58d328d479ddfaf613f4c4a39dc2de6
Keyve Sampaio <keyve10@live.com>
2022-04-17 07:07:25 -0300

    exercício de fixação: construindo uma aplicação de conta bancária.

--- ContaBancáriaExercicio/Program.cs
+++ ContaBancáriaExercicio/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ContaBancáriaExercicio
 {
@@ -10,6 +11,46 @@ namespace ContaBancáriaExercicio
     {
         static void Main(string[] args)
         {
+
+            ContaBancaria conta;
+
+            Console.Write("Entre com o número da conta:");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (S/N) ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para depósito: ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(quantia);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(conta);
+            Console.ReadKey();
         }
     }
 }
